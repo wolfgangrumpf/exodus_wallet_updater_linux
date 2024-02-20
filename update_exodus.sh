d0beabc0dcb31884f4f056f0954af9af63c620a9
Wolfgang Rumpf <wolfgang.rumpf@gmail.com>
2024-02-20 11:57:32 -0500

    Fixed download link issue
    Added the AGENT to the wget call.  Script now works again.

--- update_exodus.sh
+++ update_exodus.sh
@@ -23,8 +23,8 @@ if [ "$current" != "$installed" ]
   then
     echo "New Version Available - Installing"
     # Download version in "no clobber mode"
-    wget -nc https://downloads.exodus.com/releases/exodus-linux-x64-${current}.deb
-
+    wget --user-agent="Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)" -nc https://downloads.exodus.com/releases/exodus-linux-x64-${current}.deb
+    echo "getting https://downloads.exodus.com/releases/exodus-linux-x64-${current}.deb"
     # Now let's install
     sudo dpkg -i exodus-linux-x64-${current}.deb
     # Now cleanup
